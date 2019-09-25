8cb3bf47356f8cecf5e1a4afbc5b3bd0f287c6ad
Toby Twigger <tt15951@bristol.ac.uk>
2019-09-25 12:01:20 +0100

    Updates

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -36,7 +36,7 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    encrypt: true,    
+    encrypted: true,    
     disableStats: true,
 });
 
