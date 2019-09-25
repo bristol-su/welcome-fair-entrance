b774bec499c38ee1252ac1b84f1c477df0c7cf0e
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-09-25 12:49:31 +0100

    Update

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -34,9 +34,9 @@ Vue.prototype.$http = axios;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: window.location.hostname,
+    wsHost: process.env.MIX_PUSHER_URL,
     wsPort: 6001,
-    encrypted: true,    
+    encrypted: false,
     disableStats: true,
 });
 
