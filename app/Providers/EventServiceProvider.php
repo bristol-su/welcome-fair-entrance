4d51431d2c88dadfe5edcadd31e58f7712f0ba0e
Toby Twigger <tt15951@bristol.ac.uk>
2019-09-29 17:16:59 +0100

    Updaet

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Listeners\LogScanRequest;
+
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -30,6 +32,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         ScanUpdateRequest::class => [
             FindUidFromLibraryCard::class,
+	    LogScanRequest::class
         ],
     ];
 
